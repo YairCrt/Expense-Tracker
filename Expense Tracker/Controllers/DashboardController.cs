69b6eadc9a059306fc6e5bcb14c7f9bc74898456
Coryb <y-crbnz25@outlook.com>
2024-04-19 17:16:35 -0600

    Doughnut added

--- Expense Tracker/Controllers/DashboardController.cs
+++ Expense Tracker/Controllers/DashboardController.cs
@@ -1,11 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Expense_Tracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+using System.Globalization;
 
 namespace Expense_Tracker.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public DashboardController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+        public async Task<ActionResult> Index()
+        {
+            //Last 7 days
+            DateTime startDate = DateTime.Today.AddDays(-6);
+            DateTime endDate = DateTime.Today;
+
+            List<Transaction> SelectedTransactions = await _context.Transactions.Include(x => x.Category)
+                .Where(y => y.Date >= startDate && y.Date <= endDate).ToListAsync();
+
+            //Total Income
+            int totalIncome = SelectedTransactions
+                .Where(i => i.Category.Type == "Income")
+                .Sum(j => j.Amount);
+            ViewBag.TotalIncome = totalIncome.ToString("C0");
+
+            //Total Expense
+            int totalExpense = SelectedTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .Sum(j => j.Amount);
+            ViewBag.TotalExpense = totalExpense.ToString("C0");
+
+            //Balance
+            int balance = totalIncome - totalExpense;
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
+            culture.NumberFormat.CurrencyNegativePattern = 1;
+            ViewBag.Balance = String.Format(culture, "{0:C0}", balance);
+
+
+            //Doughnut chart - Expense by Category
+            ViewBag.DoughnutChartData = SelectedTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .GroupBy(j => j.Category.CategoryId)
+                .Select(k => new
+                {
+                    categoryTitleWithIcon = k.First().Category.Icon + " " + k.First().Category.Title,
+                    amount = k.Sum(j => j.Amount),
+                    formattedAmount = k.Sum(j => j.Amount).ToString("C0"),
+                }).ToList();
+
             return View();
         }
     }
